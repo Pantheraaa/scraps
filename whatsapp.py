1b45b7318be479d178881fb1f73498ec0c0ac44a
pantheraaa <rahul1210rk@gmail.com>
2024-05-03 11:52:22 +0530

    whatsapp auto message script

---
+++ whatsapp.py
@@ -0,0 +1,40 @@
+import pyautogui 
+# import webbrowser as wb
+import time
+# wb.open("https://web.whatsapp.com")
+time.sleep(15)
+for i in range(100):
+    pyautogui.press("E")
+    pyautogui.press("N")
+    pyautogui.press("J")
+    pyautogui.press("O")
+    pyautogui.press("Y")
+    # pyautogui.press("!")
+    # pyautogui.press("o")
+    # pyautogui.press("n")
+    # pyautogui.press("l")
+    # pyautogui.press("i")
+    # pyautogui.press("n")
+    # pyautogui.press("e")
+    pyautogui.press(" ")
+    pyautogui.press(";")
+    pyautogui.press(")")
+    pyautogui.press("enter")
+    
+
+
+
+
+    # pyautogui.press("G")
+    # pyautogui.press("o")
+    # pyautogui.press(" ")
+    # pyautogui.press("&")
+    # pyautogui.press(" ")
+    # pyautogui.press("s")
+    # pyautogui.press("t")
+    # pyautogui.press("u")
+    # pyautogui.press("d")
+    # pyautogui.press("y")
+    # pyautogui.press(" ")
+    # pyautogui.press("😴")
+    # pyautogui.press("enter")
